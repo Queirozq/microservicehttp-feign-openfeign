895e8cda56eb36646464ef6c9ca21115a7c33945
Matheus Queiroz <scririft@gmail.com>
2021-11-19 00:51:10 -0300

    Implementing eureka

---
+++ service-registry/src/test/java/com/queiroz/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queiroz.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
