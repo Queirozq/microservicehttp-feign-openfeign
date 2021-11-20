66df6038be3edac7a0337cfabe9e8e4ac5766e9f
Matheus Queiroz <scririft@gmail.com>
2021-11-19 21:02:45 -0300

    Configuring erro-based circuit breaking with Resilience4j programmatically

--- online-store-service/src/main/java/com/Queirozq/onlinestore/service/external/config/FeignConfiguration.java
+++ online-store-service/src/main/java/com/Queirozq/onlinestore/service/external/config/FeignConfiguration.java
@@ -1,11 +1,18 @@
 package com.Queirozq.onlinestore.service.external.config;
 
+import feign.Feign;
 import feign.Logger;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.openfeign.CircuitBreakerNameResolver;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+
+import java.time.Duration;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.Queirozq.onlinestore.service.external")
@@ -18,5 +25,24 @@ public class FeignConfiguration implements FeignFormatterRegistrar {
     @Bean
     public Logger.Level loggerLevel(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer(){
+        CircuitBreakerConfig cbConfig = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(5)
+                .failureRateThreshold(20.0f)
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .permittedNumberOfCallsInHalfOpenState(5)
+                .build();
+        return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory.configure(builder ->
+                builder.circuitBreakerConfig(cbConfig), "UserSessionClient#validateSession(UUID)","UserSessionClient#validateSession(Map,Map)");
+
+    }
+
+    @Bean
+    public CircuitBreakerNameResolver circuitBreakerNameResolver(){
+        return (feignClientName, target, method) -> Feign.configKey(target.type(), method);
     }
 }
