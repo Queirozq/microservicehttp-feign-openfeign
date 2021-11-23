61a0c3a751b98534314be5a2f0d5f7208e806ccb
Matheus Queiroz <scririft@gmail.com>
2021-11-22 23:57:28 -0300

    Testing Resilience4j error-based circuit breaking

--- online-store-service/src/main/java/com/Queirozq/onlinestore/service/api/SessionValidationFilter.java
+++ online-store-service/src/main/java/com/Queirozq/onlinestore/service/api/SessionValidationFilter.java
@@ -3,6 +3,7 @@ package com.Queirozq.onlinestore.service.api;
 import com.Queirozq.onlinestore.service.external.session.UserSessionClient;
 import com.Queirozq.onlinestore.service.external.session.UserSessionValidatorResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SessionValidationFilter implements Filter {
 
     private final UserSessionClient userSessionClient;
@@ -34,12 +36,17 @@ public class SessionValidationFilter implements Filter {
             if(sleepTime != null){
                 headerMap.put("X-Sleep", sleepTime);
             }
-            UUID sessionId = UUID.fromString(sessionIdHeader);
-            UserSessionValidatorResponse userSessionValidatorResponse = userSessionClient.validateSession(sessionId, headerMap);
-            if (!userSessionValidatorResponse.isValid()) {
-                httpServletResponse.sendError(HttpStatus.FORBIDDEN.value());
-            } else {
-                chain.doFilter(request, response);
+            try {
+                UUID sessionId = UUID.fromString(sessionIdHeader);
+                UserSessionValidatorResponse userSessionValidatorResponse = userSessionClient.validateSession(sessionId, headerMap);
+                if (!userSessionValidatorResponse.isValid()) {
+                    httpServletResponse.sendError(HttpStatus.FORBIDDEN.value());
+                } else {
+                    chain.doFilter(request, response);
+                }
+            } catch (Exception e) {
+                log.error("Exception while executing the session validator filter", e);
+                httpServletResponse.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
             }
         }
     }
---
+++ online-store-service/src/test/java/com/Queirozq/onlinestore/service/CircuitBreakerTest.java
@@ -0,0 +1,74 @@
+package com.Queirozq.onlinestore.service;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+
+@SpringBootTest({"server.port:0", "eureka.client.enabled:false"})
+@AutoConfigureMockMvc
+public class CircuitBreakerTest {
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        public ServiceInstanceListSupplier serviceInstanceListSupplier() {
+            return new TestServiceInstanceListSupplier("user-session-service", 8082);
+        }
+    }
+
+    @RegisterExtension
+    static WireMockExtension USER_SESSION_SERVICE = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.wireMockConfig().port(8082))
+            .build();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testErrorBasedCircuitBreaking() throws Exception {
+        String responseBody = "{ \"sessionId\": \"5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2\", \"valid\": true}";
+
+        USER_SESSION_SERVICE.stubFor(get(urlPathEqualTo("/user-sessions/validate"))
+                .withQueryParam("sessionId", equalTo("5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                .willReturn(serviceUnavailable()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health")
+                        .header("X-Session-Id", "5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                .andExpect(MockMvcResultMatchers.status().is(500));
+
+        USER_SESSION_SERVICE.stubFor(get(urlPathEqualTo("/user-sessions/validate"))
+                .withQueryParam("sessionId", equalTo("5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                .willReturn(okJson(responseBody)));
+
+        for (int i = 0; i < 4; i++) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health")
+                            .header("X-Session-Id", "5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                    .andExpect(MockMvcResultMatchers.status().is(200));
+        }
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health")
+                        .header("X-Session-Id", "5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                .andExpect(MockMvcResultMatchers.status().is(500));
+
+        Thread.sleep(6000);
+
+        for (int i = 0; i < 6; i++) {
+            mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health")
+                            .header("X-Session-Id", "5d9d5eb2-c4b3-4b6e-85fa-1d8aacf6b7b2"))
+                    .andExpect(MockMvcResultMatchers.status().is(200));
+        }
+    }
+}
